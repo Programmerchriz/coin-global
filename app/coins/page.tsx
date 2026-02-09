682642890fb59abab7926ae5abdf925ff22ffd85
ProgrammerChris6002 <ProgrammerChris6002@gmail.com>
2026-02-09 14:18:31 +0100

    fixed coins pagination

--- app/coins/page.tsx
+++ app/coins/page.tsx
@@ -1,36 +1,29 @@
-import Link from "next/link";
-import { cn, formatCurrency, formatPercentage } from "@/lib/utils";
-import { TrendingUp, TrendingDown } from "lucide-react";
-import { fetcher } from "@/lib/coingecko.actions";
-import DataTable from "../components/DataTable";
-import Image from "next/image";
-import { AllCoinsFallback } from "./fallback";
+import Image from 'next/image';
+import Link from 'next/link';
+import { TrendingUp, TrendingDown } from 'lucide-react';
 
-// market_cap_rank, (image, name(symbol)), current_price, price_change_percentage_24h, market_cap
+import { cn, formatCurrency, formatPercentage } from '@/lib/utils';
+import { fetcher } from '@/lib/coingecko.actions';
+import DataTable from '../components/DataTable';
+import CoinsPagination from '../components/CoinsPagination';
+import { AllCoinsFallback } from './fallback';
 
 const columns: DataTableColumn<CoinMarketData>[] = [
   {
     header: 'Rank',
     cellClassName: 'rank-cell',
-    cell: (coin) => `#${formatCurrency(coin.market_cap_rank, undefined, undefined, false).slice(0, -3)}`,
+    cell: (coin) =>
+      `#${formatCurrency(coin.market_cap_rank, undefined, undefined, false).slice(0, -3)}`,
   },
   {
     header: 'Token',
     cellClassName: 'token-cell',
     cell: (coin) => {
       return (
-        <Link
-          href={`/coins/${coin.id}`}
-          className="token-info"
-        >
-          <Image
-            src={coin.image}
-            alt={`${coin.name} Logo`}
-            width={28}
-            height={28}
-          />
+        <Link href={`/coins/${coin.id}`} className="token-info">
+          <Image src={coin.image} alt={`${coin.name} Logo`} width={28} height={28} />
           <p>
-              {coin.name} ({coin.symbol.toUpperCase()})
+            {coin.name} ({coin.symbol.toUpperCase()})
           </p>
         </Link>
       );
@@ -39,14 +32,17 @@ const columns: DataTableColumn<CoinMarketData>[] = [
   {
     header: 'Price',
     cellClassName: 'price-cell',
-    cell: (coin) => `${formatCurrency(coin.current_price)}`.toLowerCase().startsWith("us") ? `${formatCurrency(coin.current_price)}`.slice(2) : `${formatCurrency(coin.current_price)}`,
+    cell: (coin) =>
+      `${formatCurrency(coin.current_price)}`.toLowerCase().startsWith('us')
+        ? `${formatCurrency(coin.current_price)}`.slice(2)
+        : `${formatCurrency(coin.current_price)}`,
   },
   {
     header: '24h Change',
     cellClassName: 'change-header-cell',
     cell: (coin) => {
       const isTrendingUp = coin.price_change_percentage_24h > 0;
-      
+
       return (
         <div className={cn('change-cell', isTrendingUp ? 'text-green-500' : 'text-red-500')}>
           <p className="flex items-center gap-1">
@@ -64,44 +60,61 @@ const columns: DataTableColumn<CoinMarketData>[] = [
   {
     header: 'Market Cap',
     cellClassName: 'market-cap-cell',
-    cell: (coin) => `${formatCurrency(coin.market_cap)}`.toLowerCase().startsWith("us") ? `${formatCurrency(coin.market_cap)}`.slice(2) : `${formatCurrency(coin.market_cap)}`,
+    cell: (coin) =>
+      `${formatCurrency(coin.market_cap)}`.toLowerCase().startsWith('us')
+        ? `${formatCurrency(coin.market_cap)}`.slice(2)
+        : `${formatCurrency(coin.market_cap)}`,
   },
 ];
 
-const Coins = async () => {
-  let allCoins;
-  
-    try {
-      // allCoins = await fetcher<CoinMarketData[]>('/coins/markets?vs_currency=usd');
-      allCoins = await fetcher<CoinMarketData[]>('/coins/markets', {
-        vs_currency: "usd",
+const Coins = async ({ searchParams }: NextPageProps) => {
+  const { page } = await searchParams;
+  const currentPage = Number(page) || 1;
+  const perPage: number = 10;
+
+  let allCoins: CoinMarketData[];
+
+  try {
+    // allCoins = await fetcher<CoinMarketData[]>('/coins/markets?vs_currency=usd');
+    allCoins = await fetcher<CoinMarketData[]>(
+      '/coins/markets',
+      {
+        vs_currency: 'usd',
         order: "market_cap_desc",
-        // per_page: perPage,
-        // page: currentPage,
+        per_page: perPage,
+        page: currentPage,
         // sparkline: "false",
-        // price_change_percentage: "24h",
-      });
-    } catch (error) {
-      console.error("Error fetching categories:", error);
-      return (<AllCoinsFallback />);
-    }
+        price_change_percentage: "24h",
+      },
+    );
+    console.log(allCoins.length);
+  } catch (error) {
+    console.error('Error fetching categories:', error);
+    return <AllCoinsFallback />;
+  }
+
+  const hasMorePages = allCoins.length === perPage;
+  const estimatedTotalPages = currentPage >= 100 ? Math.ceil(currentPage / 100) * 100 + 100 : 100;
 
   return (
-    <div
-      id='coins-page'
-      className="custom-scrollbar"
-    >
+    <div id="coins-page" className="custom-scrollbar">
       <h4>All Coins</h4>
       <DataTable
-        data={allCoins.slice(0, 20)}
+        data={allCoins}
         columns={columns}
-        rowKey={(coin) => coin.id}
+        rowKey={(row) => row.id}
         tableClassName="coins-table mt-3"
         headerClassName="py-3!"
         bodyCellClassName="py-2!"
       />
+
+      <CoinsPagination
+        currentPage={currentPage}
+        totalPages={estimatedTotalPages}
+        hasMorePages={hasMorePages}
+      />
     </div>
-  )
-}
+  );
+};
 
 export default Coins;
