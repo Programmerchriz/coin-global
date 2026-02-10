47e0ea0976d0a17c311f2d7faa33f15a4f18e8c3
ProgrammerChris6002 <ProgrammerChris6002@gmail.com>
2026-02-10 05:51:16 +0100

    added back button

---
+++ app/components/BackButton.tsx
@@ -0,0 +1,28 @@
+
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { ChevronLeft } from 'lucide-react';
+
+type BackButtonProps = {
+  fallbackHref?: string;
+};
+
+export default function BackButton({ fallbackHref = '/' }: BackButtonProps) {
+  const router = useRouter();
+
+  return (
+    <button
+      onClick={() => {
+        if (window.history.length > 1) {
+          router.back();
+        } else {
+          router.push(fallbackHref);
+        }
+      }}
+      className="flex items-center hover:cursor-pointer px-2 pr-4 py-2 mb-3 rounded-lg bg-[#84cc16] text-[#0f172a] font-semibold"
+    >
+      <ChevronLeft /> <span>Back</span>
+    </button>
+  );
+};
